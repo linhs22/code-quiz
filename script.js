12282959adeccd9cf0b838f486de1412db234bed
Henry Lin <henrylin58@gmail.com>
2019-12-09 09:25:45 -0800

    added functionality of dropping score when incorrect and final score page

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ var timeEl = document.querySelector(".nav-item");
 var starter = document.querySelector(".starter");
 var quizContent = document.querySelector(".questions")
 var start = document.createElement("button");
+var lastq = document.querySelector(".section4");
+var score = document.querySelector(".score");
+var timerInterval
+
 
 var secondsLeft = 0;
 var i = 0;
 
 
+
+
 function setTime() {
   // event.preventDefault();
-  var secondsLeft = 75;
-  var timerInterval = setInterval(function () {
+  secondsLeft = 75;
+  timerInterval = setInterval(function () {
     timeEl.style.color = "white"
     secondsLeft--;
     timeEl.textContent = "Time: " + secondsLeft;
 
-    if (secondsLeft === 0) {
+    if (secondsLeft <= 0) {
       clearInterval(timerInterval);
       // alert("You ran out of time. Score is 0.")
     }
@@ -37,7 +43,18 @@ function homePage() {
   starter.style.textAlign = ("center");
 }
 
+function scoreGame() {
+  var done = document.createElement("h1");
+  done.textContent = "All done.";
+  var scoreText = document.createElement("p");
+  scoreText.textContent = "Your score is " + secondsLeft + ".";
+  score.appendChild(done);
+  score.appendChild(scoreText);
+}
 function quiz() {
+  if (i === 5) {
+    return scoreGame()
+  }
   var section = document.createElement("div");
   section.setAttribute('class', `section${i}`)
   quizContent.appendChild(section);
@@ -56,30 +73,37 @@ function quiz() {
 }
 
 
-quizContent.addEventListener("click", function(event) {
+quizContent.addEventListener("click", function (event) {
   var element = event.target;
-  console.log(element)
+  var sect = document.querySelector(`.section${i}`)
 
   // If that element is a button...
   if (element.matches("button") === true) {
     // Get its data-index value and remove the todo element from the list
-    console.log(event.target.textContent)
     var choice = event.target.textContent;
-    if(choice === questions[i].answer) {
-      console.log("Correct")
-      i++
-      quiz()
-
+    if (choice === questions[i].answer) {
+      sect.style.display = "none";
+      console.log("Correct");
+      alert("Correct!");
+      i++;
+      return quiz();
     }
     else {
-      console.log("incorrect")
+      sect.style.display = "none";
+      console.log("Incorrect");
+      alert("Incorrect!");
+      if (secondsLeft < 15) {
+        secondsLeft = 0
+      } else {
+        secondsLeft = secondsLeft - 15;
+      }
+      i++;
+      return quiz();
     }
-
-
-
-
   }
 });
+
+
 
 
 homePage();
