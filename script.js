81e4a167f4ddd60a64f1f0f525b415797a541ebe
Henry Lin <henrylin58@gmail.com>
2019-12-09 22:06:11 -0800

    Added final functionality req

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ var start = document.createElement("button");
 var lastq = document.querySelector(".section4");
 var score = document.querySelector(".score");
 var timerInterval
+var intials = document.createElement("input");
+intials.setAttribute('type', 'text')
+intials.setAttribute('class', 'initialHS')
+var submit = document.createElement("input");
+submit.setAttribute("type", "submit");
+submit.setAttribute("id", "submit")
+var high = document.querySelector(".highscore");
+var nav = document.querySelector(".navbar")
+var returnToQuiz = document.createElement("button");
+var hs = document.querySelector("#titlehead")
 
 
 var secondsLeft = 0;
 var i = 0;
+var initialsInput
+
 
 
 
@@ -30,6 +42,7 @@ function setTime() {
 }
 
 function homePage() {
+  high.style.display = "none";
   timeEl.textContent = "Time: " + secondsLeft;
   timeEl.style.color = "white"
   var title = document.createElement("h1");
@@ -49,17 +62,22 @@ function scoreGame() {
   done.textContent = "All done.";
   var scoreText = document.createElement("p");
   scoreText.textContent = "Your score is " + secondsLeft + ".";
+  var submitinit = document.createElement("p");
+  submitinit.textContent = "Enter your initials for highscore";
   score.appendChild(done);
   score.appendChild(scoreText);
+  score.appendChild(submitinit);
+  score.appendChild(intials);
+  score.appendChild(submit);
 }
 function quiz() {
   if (i === 5) {
     return scoreGame()
   }
+  starter.style.display = "none";
   var section = document.createElement("div");
   section.setAttribute('class', `section${i}`)
   quizContent.appendChild(section);
-  starter.style.display = "none";
   var quest = document.createElement("h2");
   quest.textContent = questions[i].title;
   section.appendChild(quest);
@@ -72,6 +90,49 @@ function quiz() {
     section.appendChild(row);
   }
 }
+
+function saveScoreInput(initialsInput, score) {
+  nav.style.display = "none"
+  score.style.display = "none"
+  if (initialsInput === "") {
+    alert("Initials cannot be blank");
+    return false;
+  }
+  var score = secondsLeft
+  var initialsInput = document.querySelector(".initialHS").value;
+  var existing = localStorage.getItem('users');
+  existing = existing ? JSON.parse(existing) : [];
+  existing.push({initials: initialsInput.trim(), score: score});
+  localStorage.setItem("users", JSON.stringify(existing));
+  appendWinners()
+};
+
+function winners() {
+  var existing = localStorage.getItem('users');
+  return existing ? JSON.parse(existing) : [];
+};
+
+function appendWinners() {
+  high.style.display = "block";
+  var highscore = document.createElement("h1");
+  highscore.textContent = "Highscore";
+  high.appendChild(highscore);
+  var existing = winners();
+  for (users in existing)
+  {
+      var elementName = document.createElement("div");
+      elementName.setAttribute("id", "scoreList");
+      elementName.textContent = existing[users].initials;
+      highscore.appendChild(elementName);
+      var elementScore = document.createElement("div");
+      elementScore.setAttribute("id", "scoreList");
+      elementScore.textContent = existing[users].score;
+      highscore.appendChild(elementScore);
+  } 
+  returnToQuiz.textContent="Return to Quiz";
+  highscore.appendChild(returnToQuiz);
+};
+
 
 
 quizContent.addEventListener("click", function (event) {
@@ -107,7 +168,11 @@ quizContent.addEventListener("click", function (event) {
 
 
 
-homePage();
+
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+  saveScoreInput(initialsInput,score);
+})
 
 
 
@@ -116,3 +181,26 @@ start.addEventListener("click", function (event) {
   setTime();
   quiz();
 });
+
+returnToQuiz.addEventListener("click", function (event) {
+  event.preventDefault();
+  high.style.display = "none";
+  starter.style.display = "block";
+  nav.style.display = "block";
+  $(starter).empty();
+  $(quizContent).empty();
+  $(score).empty();
+  $(high).empty();
+  
+  homePage();
+});
+
+hs.addEventListener("click", function (event) {
+  event.preventDefault();
+  appendWinners();
+  starter.style.display = "none";
+  nav.style.display = "none";
+});
+
+
+homePage();
